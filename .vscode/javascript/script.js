2e92144bc58b049f561701072ebe3274685e7776
Nelson Luiz Meirelles da Costa Júnior <nelson.costa@forlogic.net>
2024-12-20 15:03:31 -0300

    .

--- .vscode/javascript/script.js
+++ .vscode/javascript/script.js
@@ -116,7 +116,14 @@ alert ('Usuário Criado !')
 function criarLista (colaborador){
    const lista = document.querySelector(".lista-colaborador")
    const item = document.createElement("li")
-   item.innerHTML = `<p><abbr title="${colaborador.nome}">${colaborador.nome}</abbr> </p> <p><abbr title="${colaborador.email}">${colaborador.email}</abbr> </p> <p>${colaborador.status?"ativo": "inativo"}</p>`
+   item.innerHTML = `<p><abbr title="${colaborador.nome}">${colaborador.nome}</abbr> </p> <p><abbr title="${colaborador.email}">${colaborador.email}</abbr> </p> <p>${colaborador.status?"ativo": "inativo"}  </p> <p>
+         <button id="editar">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000" ><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
+         </button>
+       <button>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000" m><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/${onclick = alert('CLicou')}></svg>
+             </p>
+       </button>`
    lista.appendChild(item)
 }
 // Comando para percorrer e pegar cada array para ser colocado na lista 
@@ -173,7 +180,13 @@ async function pesquisarInput(){
 function imprimir(){
    window.print()
 }
-
+async function deletar(){
+const response = await fetch('https://localhost:7034/api/Colaboradores');
+if (response.status == 200){
+   const obj = await(response.json());
+   console.log(obj);
+}
+}
 
 
 
