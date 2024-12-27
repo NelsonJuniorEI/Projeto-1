e5ce20ffdee8c707b92781d2b805cd8db9fea157
Nelson Luiz Meirelles da Costa Júnior <nelson.costa@forlogic.net>
2024-12-27 13:45:59 -0300

    alterando

--- .vscode/javascript/script.js
+++ .vscode/javascript/script.js
@@ -132,7 +132,7 @@ alert ('Usuário Criado !')
 function criarLista (colaborador){
    const lista = document.querySelector(".lista-colaborador")
    const item = document.createElement("li")
-   item.innerHTML = `<p><abbr title="${colaborador.nome}">${colaborador.nome}</abbr> </p> <p><abbr title="${colaborador.email}">${colaborador.email}</abbr> </p> <p>${colaborador.status?"ativo": "inativo"}  </p> 
+   item.innerHTML = `<p><abbr title="${colaborador.nome}">${colaborador.nome}</abbr> </p> <p><abbr title="${colaborador.email}">${colaborador.email}</abbr> </p> <p>${colaborador.status?"Ativo": "Inativo"}  </p> 
          <div id="btns">
             <button id="editar">
                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000" ><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
@@ -177,7 +177,7 @@ catch(error)
 
 }
 async function editar(event){
- debugger
+ 
    event.preventDefault()
    let url = new URL(window.location.href)
    const parametro = url.searchParams;
@@ -320,10 +320,6 @@ if (response.status == 200){
    const obj = await(response.json());
    console.log(obj);
 }
-}
-function clicou(event){
-   event.preventDefault()
-   alert('clicou')
 }
 
 
